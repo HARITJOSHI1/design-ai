b459ec87416af50cdb6edef38dcaed2f0a90d1b4
HARITJOSHI1 <joshiharit3@gmail.com>
2026-05-05 13:01:09 +0530

    next conf update for prisma client

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    optimizePackageImports: [
+      '@prisma/client',
+    ],
+  }
 };
 
 export default nextConfig;
