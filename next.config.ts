bf6f5b25748703d91ae6300911c2d15dcaf4c798
HARITJOSHI1 <joshiharit3@gmail.com>
2026-05-07 13:01:15 +0530

    fix v3

--- next.config.ts
+++ next.config.ts
@@ -2,21 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
-  env: {
-    KINDE_SITE_URL: process.env.KINDE_SITE_URL ?? `https://${process.env.VERCEL_URL}`,
-    KINDE_POST_LOGOUT_REDIRECT_URL:
-      process.env.KINDE_POST_LOGOUT_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}`,
-    KINDE_POST_LOGIN_REDIRECT_URL:
-      process.env.KINDE_POST_LOGIN_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}`
-  },
-
   outputFileTracingIncludes: {
-
-    '/api/**/*': ['./node_modules/.prisma/client/**/*'],
-
-    '/*': ['./node_modules/.prisma/client/**/*'],
-
+    '/api/**/*': [
+      './node_modules/.prisma/client/**/*',
+      './lib/generated/prisma/**/*',
+    ],
+    '/*': [
+      './node_modules/.prisma/client/**/*',
+      './lib/generated/prisma/**/*',
+    ],
   },
 };
 
